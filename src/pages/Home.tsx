ba683012d9e2ebb084e0b78bf1b8665280d6d174
Zachary Yu <zyu31415@gmail.com>
2025-09-23 20:14:15 -0400

    stuff

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -45,7 +45,7 @@ export const Home = () => {
                 <h1 className="text-4xl md:text-6xl font-bold text-foreground square-font leading-tight">
                   <div className="opacity-line">THE HACKATHON</div>
                   <div className="opacity-line">THAT'S SOLVING</div>
-                  <div className="opacity-line">THE WORLD'S WEIRDEST PROBLEMS.</div>
+                  <div className="opacity-line">THE WORLD'S WEIRDEST PROBLEMS</div>
                 </h1>
               </div>
 
