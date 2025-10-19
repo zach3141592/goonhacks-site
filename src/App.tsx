a97783c407f37b03b44d3a22f1d8fe049afb0f47
Zachary Yu <zyu31415@gmail.com>
2025-10-19 02:36:08 -0400

    analytics

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Analytics } from '@vercel/analytics/react';
 import Home from "./pages/Home";
 
 const queryClient = new QueryClient();
@@ -12,6 +13,7 @@ const App = () => (
       <Toaster />
       <Sonner />
       <Home />
+      <Analytics />
     </TooltipProvider>
   </QueryClientProvider>
 );
