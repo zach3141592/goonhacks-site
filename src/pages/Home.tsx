fa676b030b5656324c96c5b72952bfc7e2e36fda
Zachary Yu <zyu31415@gmail.com>
2025-11-06 14:57:28 -0500

    add schedule

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -179,7 +179,7 @@ export const Home = () => {
                     <div className="flex flex-col md:flex-row md:items-center gap-x-1 gap-y-1">
                       <span className="font-bold text-lg md:text-xl">12:00 am</span>
                       <span className="text-lg md:text-xl hidden md:inline"> → </span>
-                      <span className="text-lg">Snacks</span>
+                      <span className="text-lg">Late Night Snacks</span>
                     </div>
                   </div>
                 </div>
